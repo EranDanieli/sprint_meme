b8d5b50de6e4f622193172ef8b6b84f41df03f71
EranDanieli <87779779+EranDanieli@users.noreply.github.com>
2021-08-12 18:52:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,99 +1,92 @@
 'use strict'
-
+// let gFont  TODO
 let gCanvas;
+var gImgs =[];
 let gCtx;
-var gMeme = {
-    selectedImgId: 5,
+var gCurrMeme = {
+   id: 5,
     selectedLineIdx: 0,
     lines: [
     {
-    txt: 'I never eat Falafel',
-    size: 20,
-    align: 'left',
-    color: 'red'
+        x:250,
+        y:20,
+        font: 'impact',
+    txt: '',
+    size: 60,
+    align: 'center',
+    color: 'white'
+    },
+    {
+        x:250,
+        y:410,
+    txt: '',
+    font: 'impact',
+    size: 60,
+    align: 'center',
+    color: 'white'
+    },
+    {
+        x:250,
+        y:220,
+    txt: '',
+    font: 'impact',
+    size: 60,
+    align: 'center',
+    color: 'white'
     }
     ]
 }
 
+
+
+
 function onInit(){
-    gCanvas = document.getElementById('my-canvas')
-    gCtx = gCanvas.getContext('2d')
-    document.querySelector('.modal').style.display='none'
-    // update()
-}
-
-
-
-function drawText(txt, x=100, y=100) {
-    console.log('txt in drawText',txt,x,y)
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = 'white'
-    gCtx.font = '60px impact'
-    gCtx.fillText(txt, x, y)
-    gCtx.strokeText(txt, x, y)
-}
-
-
-function drawImg(img) {
+    init()
     
-    var elImg = img
-    gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height)
+}
+function onAddLine(){
+    addLine()
 }
 
+function onResetLineIdx(){
+    resetLineIdx();
+}
+function onChangeColor(ev){
+    changeColor(ev.value)
+    renderCanvas()
+   
+}
+
+function onSetText(ev){
+    setText(ev)
+    renderCanvas()
+}
+
+
+function onChangeLine(){
+changeLine()
+renderCanvas()
+}
 function onOpenModal(ev){
-    document.querySelector('.search-gallery-container').style.display='none'
-    document.querySelector('.modal').style.display = 'flex' ;
-    console.log('this',ev.target)
-    drawImg(ev.target)
-}
+   openModal(ev)
+    }
+
+  
+    function onAlignTxt(ev){
+        alignTxt(ev.value)
+        renderCanvas()
+    }
+
+   
 function onGalleryClick(){
-    document.querySelector('.search-gallery-container').style.display='block'
+    document.querySelector('.gallery').style.display='block'
     document.querySelector('.modal').style.display='none'
 }
-
-// function update() {
-    //     var canvasNode = document.getElementById('my-canvas');
-    //     canvasNode.width = canvasNode.parentNode.clientWidth;
-    //     canvasNode.height = canvasNode.parentNode.clientHeight;
-    // }
-    function downloadImg(elLink) {
-        var imgContent = gCanvas.toDataURL('image/jpeg')
-        elLink.href = imgContent
-    }
-    function uploadImg() {
-        const imgDataUrl = gCanvas.toDataURL("image/jpeg");
-    
-        // A function to be called if request succeeds
-        function onSuccess(uploadedImgUrl) {
-            const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
-            document.querySelector('.user-msg').innerText = `${uploadedImgUrl}`
-    
-            document.querySelector('.share-container').innerHTML = `
-            <a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
-               Share   
-            </a>`
-        }
-        doUploadImg(imgDataUrl, onSuccess);
-    }
-    
-    function doUploadImg(imgDataUrl, onSuccess) {
-    
-        const formData = new FormData();
-        formData.append('img', imgDataUrl)
-    
-        fetch('//ca-upload.com/here/upload.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(res => res.text())
-        .then((url)=>{
-            console.log('Got back live url:', url);
-            onSuccess(url)
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-    }
-    
-    
+function onRaiseSize(){
+    raiseSize()
+    renderCanvas()
+}
+function onLowerSize(){
+    lowerSize()
+    renderCanvas()
+}
